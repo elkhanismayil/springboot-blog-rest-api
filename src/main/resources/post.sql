5d283dbbd4621c8f64528e299aa9ba14998459f5
elkhanismayil <elxan310@gmail.com>
2022-02-15 20:25:28 +0400

    post.sql added

---
+++ src/main/resources/post.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS posts;
+
+INSERT INTO posts (title, content, description) VALUES ('Number headlines', 'Any headline that starts with a number', 'introducing a list post');
+INSERT INTO posts (title, content, description) VALUES ('How to headlines', 'This is a simple option, but a good one', 'Description2');
+INSERT INTO posts (title, content, description) VALUES ('Famous comparison', 'Depending on the famous thing or person you choose', 'Description3');
+INSERT INTO posts (title, content, description) VALUES ('Scarcity headline', 'Headlines that start with “The Secret of…”', 'Description4');
+INSERT INTO posts (title, content, description) VALUES ('Big promises headline', 'this category includes headlines that start', 'Description5');
+INSERT INTO posts (title, content, description) VALUES ('The Future of Freelance Writing in 2022', 'Blog Post Titles', 'Description6');
